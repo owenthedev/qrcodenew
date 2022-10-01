7fd01e0bc537b50e518a15e8d1fbbe7d16007ce1
Owen Nxumalo <osnn@Kelseys-MacBook-Air.local>
2022-10-01 13:14:48 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+
+</script>
+
+
+<body>
+    <center> <section>
+
+        <center>
+        
+            <card>
+               <img src="image-qr-code.png" alt=""> 
+            <h1>Improve your front-end <br> skills by building projects</h1>
+            <p>Scan the QR code to visit Frontend <br> Mentor and take your coding skills <br> to the next level</p>
+            
+        
+    </card>
+    </center>
+    </section></center>
+   
+    
+        
+    
+    </body>
+
+
+<style>
+card{
+   background-color: white;
+    
+}
+body{
+    padding-top: 40px;
+    width: 1443px;
+    height: 800px;
+    background-color: hsla(219, 100%, 83%, 0.635);
+}
+section{
+    width:320px;
+    align-self: center;
+    padding: 20px;
+    background-color: white;
+    color: white;
+    border-radius: 20px;
+}
+h1{
+    color: hsl(218, 44%, 22%);
+}
+p{
+    color: hsl(220, 15%, 55%);
+    align-self: center;
+}
+img {
+    width: 300px;
+    border-radius: 10px;
+}
+
+</style>
